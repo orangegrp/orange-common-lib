bea3ef2536c31ce6651cf690c902777b55f4596c
alex <88835216+a4004@users.noreply.github.com>
2024-07-20 19:45:59 +0100

    environment var manager + updated logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export * from "./logger.js"
-
+export * from "./envcfg.js";
 export * as ConfigApi from "./configApiTypes/api_v1.js"
+
+export function sleep(time: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
+}
