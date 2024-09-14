432025fa130fc16c49d2fa959d3282c2f9643290
Alex <88835216+a4004@users.noreply.github.com>
2024-09-14 12:45:46 +0000

    more logs + resilience to crashes

--- src/envcfg.ts
+++ src/envcfg.ts
@@ -1,82 +1,107 @@
 import dotenv from "dotenv";
 import chalk from "chalk";
+import util from "util";
 import { createClient } from "@supabase/supabase-js";
 
 const envData: OrangeEnvCfg = {};
 
 function log(msg: string, level: "Verbose" | "Log" | "Error" | "Warning" | "Success" = "Verbose") {
-    if (level == "Verbose") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[MINILOG]"), chalk.gray(msg));
-    if (level == "Log") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[MINILOG]"), chalk.white(msg));
-    if (level == "Error") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[MINILOG]"), chalk.red(msg));
-    if (level == "Warning") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[MINILOG]"), chalk.yellow(msg));
-    if (level == "Success") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[MINILOG]"), chalk.green(msg));
+    if (level == "Verbose") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[ENVINIT]"), chalk.gray(msg));
+    if (level == "Log") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[ENVINIT]"), chalk.white(msg));
+    if (level == "Error") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[ENVINIT]"), chalk.red(msg));
+    if (level == "Warning") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[ENVINIT]"), chalk.yellow(msg));
+    if (level == "Success") console.log(chalk.cyan(`[ ${new Date().toISOString()}]`), chalk.gray("[ENVINIT]"), chalk.green(msg));
+}
+
+function initResilience() {
+    log(`Setting up uncaughtException handler...`, "Log");
+    process.on("uncaughtException", (err) => {
+        log(`Uncaught exception: ${util.inspect(err, { depth: null})}`, "Error");
+    });
+    log(`uncaughtException handler registered!`, "Success");
+    
+    log(`Setting up unhandledRejection handler...`, "Log");
+    process.on("unhandledRejection", (err) => {
+        log(`Unhandled rejection: ${util.inspect(err, { depth: null})}`, "Error");
+    });
+    log(`unhandledRejection handler registered!`, "Success");
 }
 
 function initEnv(): Promise<boolean> {
     return new Promise(async (resolve, reject) => {
-        if (process.env) {
-            let envs = [];
-            let counter = 0;
-            for (const key in process.env) {
-                const envValue = process.env[key];
-                if (envValue) {
-                    envData[key] = envValue;
+        try {
+            if (process.env) {
+                let envs = [];
+                let counter = 0;
+                for (const key in process.env) {
+                    const envValue = process.env[key];
+                    if (envValue) {
+                        envData[key] = envValue;
+                        envs.push(key);
+                        counter++;
+                    }
+                }
+                log(`Imported environment variables ${envs.join(", ")}`, "Verbose");
+                log(`Loaded ${counter} environment variables!`, "Success");
+            }
+    
+            const localVars = dotenv.config();
+    
+            if (localVars.parsed) {
+                let envs = [];
+                let counter = 0;
+                for (const key in localVars.parsed) {
+                    process.env[key] = envData[key] = localVars.parsed[key];
                     envs.push(key);
                     counter++;
                 }
+                log(`Imported local environment variables ${envs.join(", ")}`, "Verbose");
+                log(`Loaded ${counter} local (.env) environment variables!`, "Success");
             }
-            log(`Imported environment variables ${envs.join(", ")}`, "Verbose");
-            log(`Loaded ${counter} environment variables!`, "Success");
-        }
-
-        const localVars = dotenv.config();
-
-        if (localVars.parsed) {
-            let envs = [];
-            let counter = 0;
-            for (const key in localVars.parsed) {
-                process.env[key] = envData[key] = localVars.parsed[key];
-                envs.push(key);
-                counter++;
-            }
-            log(`Imported local environment variables ${envs.join(", ")}`, "Verbose");
-            log(`Loaded ${counter} local (.env) environment variables!`, "Success");
-        }
-
-        if (!process.env.SUPABASE_SERVER) { log("SUPABASE_SERVER is not set!", "Error"); reject(false); return; };
-        if (!process.env.SUPABASE_ANON_KEY) { log("SUPABASE_ANON_KEY is not set!", "Error"); reject(false); return; };
-
-        const supabase = createClient(process.env.SUPABASE_SERVER, process.env.SUPABASE_ANON_KEY);
-
-        if (!supabase) { log("Failed to connect to Supabase!", "Error"); reject(false); return; };
-        if (!process.env.SUPABASE_USERNAME) { log("SUPABASE_USERNAME is not set!", "Error"); reject(false); return; };
-        if (!process.env.SUPABASE_PASSWORD) { log("SUPABASE_PASSWORD is not set!", "Error"); reject(false); return; };
-
-        const { error: signInError } = await supabase.auth.signInWithPassword({ email: process.env.SUPABASE_USERNAME, password: process.env.SUPABASE_PASSWORD });
-
-        if (signInError) { log(`Failed to sign in to Supabase!`, "Error"); reject(signInError); return; }
-
-        const { data: remoteVars, error } = await supabase.from(`orange_bot_environment_${process.env.NODE_ENV === "production" ? "prod" : "dev"}`).select("*");
-
-        if (error) { log("Failed to load remote (Supabase) environment variables!", "Error"); reject(error); return; }
-
-        if (remoteVars) {
-            let envs = [];
-            let counter = 0;
-            for (const { key, value } of remoteVars) {
-                if (process.env[key] !== undefined) {
-                    log(`Remote environment variable conflicts with local one. The local one will take precedence: ${key}`, "Warning");
-                    continue;
+    
+            if (!process.env.SUPABASE_SERVER) { log("SUPABASE_SERVER is not set!", "Error"); reject(false); return; };
+            if (!process.env.SUPABASE_ANON_KEY) { log("SUPABASE_ANON_KEY is not set!", "Error"); reject(false); return; };
+    
+            const supabase = createClient(process.env.SUPABASE_SERVER, process.env.SUPABASE_ANON_KEY);
+    
+            if (!supabase) { log("Failed to connect to Supabase!", "Error"); reject(false); return; };
+            if (!process.env.SUPABASE_USERNAME) { log("SUPABASE_USERNAME is not set!", "Error"); reject(false); return; };
+            if (!process.env.SUPABASE_PASSWORD) { log("SUPABASE_PASSWORD is not set!", "Error"); reject(false); return; };
+    
+            const { error: signInError } = await supabase.auth.signInWithPassword({ email: process.env.SUPABASE_USERNAME, password: process.env.SUPABASE_PASSWORD });
+    
+            if (signInError) { log(`Failed to sign in to Supabase!`, "Error"); reject(signInError); return; }
+    
+            const { data: remoteVars, error } = await supabase.from(`orange_bot_environment_${process.env.NODE_ENV === "production" ? "prod" : "dev"}`).select("*");
+    
+            if (error) { log("Failed to load remote (Supabase) environment variables!", "Error"); reject(error); return; }
+    
+            if (remoteVars) {
+                let envs = [];
+                let counter = 0;
+                for (const { key, value } of remoteVars) {
+                    if (process.env[key] !== undefined) {
+                        log(`Remote environment variable conflicts with local one. The local one will take precedence: ${key}`, "Warning");
+                        continue;
+                    }
+                    process.env[key] = envData[key] = value;
+                    envs.push(key);
+                    counter++;
                 }
-                process.env[key] = envData[key] = value;
-                envs.push(key);
-                counter++;
+                log(`Imported remote environment variables ${envs.join(", ")}`, "Verbose");
+                log(`Loaded ${counter} remote environment variables!`, "Success");
             }
-            log(`Imported remote environment variables ${envs.join(", ")}`, "Verbose");
-            log(`Loaded ${counter} remote environment variables!`, "Success");
+            log(`Environment configuration success!`, "Success");
+            resolve(true);
+        } catch (error) {
+            log(`Environment configuration encountered a problem!`, "Warning");
+            reject(error);
+        } finally {
+            log(`Setting up resilience...`, "Verbose");
+            initResilience();
+            log(`Resilience parameters configured!`, "Success");
+            log(`Environment configuration completed.`, "Log");
         }
-
-        resolve(true);
     });
 }
 
